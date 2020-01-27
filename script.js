d50061c71aefae912e9aaef6a87ef35c5a403f8b
SecondThundeR <vladgrom2206.com@gmail.com>
2020-01-27 23:18:02 +0300

    Hude Update
    Main Page:
     HTML:
      - Changed tab space
      - Added upper headline
      - Changed position of main title and avatar
      - Changed some text for title
      - Added new button for Project Hades Page
      - Changed some ids to classes
     CSS:
      - Now video is more blurrrrry :new_moon_with_face:
      - Clean up and rearranging of some elements in the code structure
      - There are quite a lot of changes that make me feel headache, so see them yourself, please :(
     JavaScript:
      - Changed <span id...> to <span class...>
    404 Page:
     HTML:
      - Changed title of page
      - Added new favicon
      - Added script for random quote after each page reload
      - Changed some ids to classes
      - Added sad emoticon
     CSS:
      - Added some rules for phones landscape
      - Clean up and rearranging of some elements in the code structure
     JavaScript:
      - Initial release of random quotes function
    Project Hades Page:
     - Initial Release (Maybe be a bit unstable)

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ TxtType.prototype.tick = function() {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
 
-    this.el.innerHTML = '<span id="wrap">'+this.txt+'</span>';
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
     let that = this;
     let delta = 200 - Math.random() * 100;
