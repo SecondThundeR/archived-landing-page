a7b6d7892d093eb14d539ec12f4dfcaf3eeb60e3
SecondThundeR <awayfromgalaxy@gmail.com>
2020-01-29 20:24:40 +0300

    Fixes

--- script_404.js
+++ script_404.js
@@ -7,7 +7,7 @@ let fourZeroFourWords = [
     'Aristotle once said: "If you wrote the wrong link, then you will definitely see the 404 error" But we are not sure exactly whether he said it or not',
     "Hi, this is me - error 404!. I'm glad to meet you, but probably you are not very happy :(",
     "Emptiness ... This is what you feel here, isn't it? But this is not forever :)",
-    "We can endlessly look at 3 things: how the fire burns, how the water flows and on this page",
+    "We can endlessly look at 3 things: how the fire burns, how the water flows and how this error looks on this page",
     "404 - these 3 numbers mean a lot. In this case, it means that you went to a non-existing page"];
 
 function testFun() {
