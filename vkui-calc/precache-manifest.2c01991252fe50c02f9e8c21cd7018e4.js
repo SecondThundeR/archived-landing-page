41078305ae90d7c246bc1ddfdbcdc1a2773ea405
SecondThundeR <awayfromgalaxy@gmail.com>
2020-03-27 11:25:28 +0300

    [Beta] Added VK UI Calculator

---
+++ vkui-calc/precache-manifest.2c01991252fe50c02f9e8c21cd7018e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f93fcfbdbcda106406cbf180516e14",
+    "url": "/vkui-calc/index.html"
+  },
+  {
+    "revision": "898a7b31485cf5e58cdf",
+    "url": "/vkui-calc/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "898a7b31485cf5e58cdf",
+    "url": "/vkui-calc/static/js/2.50774da8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vkui-calc/static/js/2.50774da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22bb7bf84052c9adfdb5",
+    "url": "/vkui-calc/static/js/main.87e3905c.chunk.js"
+  },
+  {
+    "revision": "99e59037526210db72c7",
+    "url": "/vkui-calc/static/js/runtime-main.13906fa8.js"
+  }
+]);
